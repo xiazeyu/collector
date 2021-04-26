adfa7efbc048760a6ab72371f72f91e5754ba819
xiazeyu <xiazeyu_2011@126.com>
2021-04-27 01:32:51 +0800

    bootstrap

--- app/db_templates/missions/mission5.py
+++ app/db_templates/missions/mission5.py
@@ -61,14 +61,28 @@ def list_zip(obj):
         str: HTML output
     """
 
-    head = '''<table border="1"><tr>
+    head = '''<!doctype html><html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+        integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.3/dist/css/bootstrap-theme.min.css"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.3/examples/theme/theme.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container theme-showcase" role="main">
+    <div class="col-md"><table class="table table-striped">
+    <thead>
     <th>文件名称</th>
     <th>修改的时间日期</th>
     <th>未压缩文件的大小</th>
     <th>已压缩数据的大小</th>
     <th>未压缩文件的 CRC-32</th>
-    </tr>'''
-
+    </tr></thead><tbody>'''
     body = ''
     for info_obj in obj.infolist():
         fileinfo = FileInfo(filename=info_obj.filename,
@@ -84,6 +98,13 @@ def list_zip(obj):
         <td>{fileinfo.compress_size.human_readable()}</td>
         <td>{fileinfo.CRC}</td>
         </tr>'''
-
-    foot = '</table>'
+    foot = '''</tbody></table></div></div></body>
+<script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"
+    integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
+    crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
+    crossorigin="anonymous"></script>
+</body></html>
+'''
     return head + body + foot
