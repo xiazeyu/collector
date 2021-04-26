0975dcc4277e9b463c56dd52f9528c7647a7bf3d
xiazeyu <xiazeyu_2011@126.com>
2021-04-27 01:55:42 +0800

    better style

--- app/db_templates/missions/mission5.py
+++ app/db_templates/missions/mission5.py
@@ -25,13 +25,29 @@ def main(file_path: Path) -> str:
     Returns:
         str: HTML output
     """
+    head = '''<!doctype html><html lang="zh-CN"><head><meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
+    integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.3/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.3/examples/theme/theme.css" rel="stylesheet">
+    </head><body><div class="container theme-showcase" role="main"><div class="col-md">'''
+
+    foot = '''</div></div><script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"
+    integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
+    crossorigin="anonymous"></script><script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
+    crossorigin="anonymous"></script></body></html>'''
+
     try:
         file = zipfile.ZipFile(file_path)
     except zipfile.BadZipFile:
-        return '压缩包已损坏，请重新打包上传。<br>'
+        return head + '<p>压缩包已损坏，请重新打包上传。</p>' + foot
+
     output = test_zip(file)
     output += list_zip(file)
-    return output
+    return head + output + foot
 
 
 def test_zip(obj: zipfile.ZipFile):
@@ -46,8 +62,8 @@ def test_zip(obj: zipfile.ZipFile):
     """
     result = obj.testzip()
     if result:
-        return '压缩包已损坏，请重新打包上传。<br>'
-    return '压缩包文件完好。<br>'
+        return '<p>压缩包已损坏，请重新打包上传。</p>'
+    return '<p>压缩包文件完好。</p>'
 
 
 def list_zip(obj):
@@ -61,22 +77,8 @@ def list_zip(obj):
         str: HTML output
     """
 
-    head = '''<!doctype html><html lang="zh-CN">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
-        integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
-    <link href="https://cdn.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.3/dist/css/bootstrap-theme.min.css"
-        rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/@bootcss/v3.bootcss.com@1.0.3/examples/theme/theme.css" rel="stylesheet">
-</head>
-
-<body>
-    <div class="container theme-showcase" role="main">
-    <div class="col-md"><table class="table table-striped">
-    <thead>
+    head = '''
+    <table class="table table-striped"><thead>
     <th>文件名称</th>
     <th>修改的时间日期</th>
     <th>未压缩文件的大小</th>
@@ -90,7 +92,7 @@ def list_zip(obj):
                             file_size=info_obj.file_size,
                             compress_size=info_obj.compress_size,
                             CRC=info_obj.CRC,
-        )
+                            )
         body += f'''<tr>
         <td>{fileinfo.filename}</td>
         <td>{fileinfo.date_time}</td>
@@ -98,13 +100,5 @@ def list_zip(obj):
         <td>{fileinfo.compress_size.human_readable()}</td>
         <td>{fileinfo.CRC}</td>
         </tr>'''
-    foot = '''</tbody></table></div></div></body>
-<script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"
-    integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ"
-    crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
-    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
-    crossorigin="anonymous"></script>
-</body></html>
-'''
+    foot = '''</tbody></table>'''
     return head + body + foot
